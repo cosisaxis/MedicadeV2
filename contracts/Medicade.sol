fb66a934a5233c12e60a4b8282191d8ca3e7989a
cosisaxis <kishanelewis2@gmail.com>
2023-07-25 07:56:34 -0500

    added access control functionality

--- contracts/Medicade.sol
+++ contracts/Medicade.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract MedicalRecordsContract {
-    // Structure to represent medical records
+    
+    address private contractOwner;
+
+    
     struct MedicalRecord {
         uint256 recordId;
         string patientName;
@@ -14,32 +17,24 @@ contract MedicalRecordsContract {
         address creator;
     }
 
-    // Array to store all medical records
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+  
     MedicalRecord[] public medicalRecords;
-
-    // Mapping to keep track of patient-specific records
     mapping(address => uint256[]) private patientRecords;
-
-    // Event to log new medical records
     event MedicalRecordAdded(uint256 indexed recordId, address indexed patientAddress);
 
-    // Modifier to ensure only authorized personnel can add medical records
+    
     modifier onlyAuthorizedPersonnel() {
-        // Add your access control logic here, e.g., check for roles, permissions, etc.
+        require(msg.sender == contractOwner, "You cannot do this");
         _;
     }
 
-    // Function to add a new medical record
-    function addMedicalRecord(
-        string memory _patientName,
-        string memory _diagnosis,
-        string memory _prescription,
-        string memory _gender,
-        uint256 _age
-    ) public onlyAuthorizedPersonnel {
+    
+    function addMedicalRecord( string memory _patientName, string memory _diagnosis, string memory _prescription, string memory _gender, uint256 _age) public onlyAuthorizedPersonnel {
         uint256 recordId = medicalRecords.length;
-
-        // Create a new MedicalRecord struct
         MedicalRecord memory newRecord = MedicalRecord({
             recordId: recordId,
             patientName: _patientName,
@@ -51,15 +46,13 @@ contract MedicalRecordsContract {
             creator: msg.sender
         });
 
-        // Store the record in the array and map it to the patient
+        
         medicalRecords.push(newRecord);
         patientRecords[msg.sender].push(recordId);
-
-        // Emit an event to log the addition of a new medical record
         emit MedicalRecordAdded(recordId, msg.sender);
     }
 
-    // Function to retrieve all medical records of a patient
+    
     function getPatientMedicalRecords() public view returns (MedicalRecord[] memory) {
         uint256[] memory patientRecordIds = patientRecords[msg.sender];
         MedicalRecord[] memory records = new MedicalRecord[](patientRecordIds.length);
@@ -67,7 +60,6 @@ contract MedicalRecordsContract {
         for (uint256 i = 0; i < patientRecordIds.length; i++) {
             records[i] = medicalRecords[patientRecordIds[i]];
         }
-
         return records;
     }
 }
